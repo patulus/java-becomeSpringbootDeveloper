dd699d79de0a15eff34f9f87ab869ad05ed975dd
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2024-05-13 20:10:16 +0900

    init

---
+++ src/main/java/com/patulus/becomeSpringDeveloper/BecomeSpringDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.patulus.becomeSpringDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecomeSpringDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecomeSpringDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patulus/becomeSpringDeveloper/BecomeSpringDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patulus.becomeSpringDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecomeSpringDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
